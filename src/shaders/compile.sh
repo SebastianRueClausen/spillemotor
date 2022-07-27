8c7627c8aebaaddc8d74f34a221329bdcc5fe8b6
SebastianRueClausen <seba474b@gmail.com>
2022-07-27 11:46:16 +0200

    Moved shader source files to seperate folder. This seperates compiled .spv files from glsl

---
+++ src/shaders/compile.sh
@@ -0,0 +1,11 @@
+glslc vert.glsl -o ../../shaders/vert.spv
+glslc frag.glsl -o ../../shaders/frag.spv
+
+glslc sdf_vert.glsl -o ../../shaders/sdf_vert.spv
+glslc sdf_frag.glsl -o ../../shaders/sdf_frag.spv
+
+glslc cluster_build.glsl -o ../../shaders/cluster_build.spv
+glslc cluster_update.glsl -o ../../shaders/cluster_update.spv
+glslc light_update.glsl -o ../../shaders/light_update.spv
+
+
